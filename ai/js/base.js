fa3910261b0955cda284c08b57e92f296bae7416
Murat Hasdemir <murathasdemir0@gmail.com>
2023-07-08 18:46:04 +0300

    yup

--- ai/js/base.js
+++ ai/js/base.js
@@ -21,20 +21,5 @@ $(document).ready(function() {
         // add active class to clicked tab link and its corresponding tab
         $(this).addClass('active');
         $($(this).attr('href')).addClass('active');
-    });
-    // Add a class to the gallery container to easily target it
-    const galleryContainer = document.querySelector('#container');
-
-    // Create a function to show the gallery and hide the preloader
-    function showGallery() {
-      galleryContainer.style.display = 'block';
-      // Remove the preloader element or hide it
-      // depending on your implementation
-    }
-
-    // Use the window.onload event to ensure all resources are loaded
-    window.onload = function() {
-      // Call the showGallery function to display the gallery
-      showGallery();
-    };
+    }); 
 });
